6d1f5d95f84214e0999e6e7e4dfb68a44abe71c6
Jørgen Sverre Lien Sellæg <jorgen.sellag@ibooking.no>
2022-11-19 14:35:39 +0100

    download the chains on first start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 chown -R swap_user "$DATADIRS"
-exec gosu swap_user "$@"
 
+if [ "$COINS" != "" && "$CURRENT_XMR_HEIGHT" != "" ] \
+     && ! [ -d /coindata/particl ]; then
+  gosu swap_user basicswap-prepare --datadir=/coindata --withcoins="$COINS" --htmlhost="0.0.0.0" --wshost="0.0.0.0" --xmrrestoreheight="$CURRENT_XMR_HEIGHT"
+fi
+
+gosu swap_user "$@"
