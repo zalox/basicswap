21eadfa4c2dd14b7fc019fc156e26cd2ddbe0aab
Jørgen Sverre Lien Sellæg <jorgen.sellag@ibooking.no>
2022-11-19 16:38:27 +0100

    simple self explaining startup script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,9 +3,16 @@ set -e
 
 chown -R swap_user "$DATADIRS"
 
-if [ "$COINS" != "" && "$CURRENT_XMR_HEIGHT" != "" ] \
-     && ! [ -d /coindata/particl ]; then
-  gosu swap_user basicswap-prepare --datadir=/coindata --withcoins="$COINS" --htmlhost="0.0.0.0" --wshost="0.0.0.0" --xmrrestoreheight="$CURRENT_XMR_HEIGHT"
+if [ "$WITH_COINS" != "" ] \
+     && [ "$CURRENT_XMR_HEIGHT" != "" ] \
+     && ! [ -d /coindata/particl ]
+then
+  gosu swap_user basicswap-prepare \
+       --datadir=/coindata \
+       --withcoins=$WITH_COINS \
+       --htmlhost="0.0.0.0" \
+       --wshost="0.0.0.0" \
+       --xmrrestoreheight=$CURRENT_XMR_HEIGHT
 fi
 
 gosu swap_user "$@"
